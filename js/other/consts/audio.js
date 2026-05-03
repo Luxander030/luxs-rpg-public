18890e2096c1188a26b88c492d87408166a1260b
Luxander <markus.khilko@gmail.com>
2026-05-03 14:43:20 -0500

    fixed audio path bug with fireball spell

--- js/other/consts/audio.js
+++ js/other/consts/audio.js
@@ -52,7 +52,7 @@ function playIceshockSFX() {
     iceshockSFX.play().catch(e => console.log("Audio playback prevented:", e));
 }
 
-const fireballSFX = new Audio("sfx/player_sfx/player_attack/player_fireball.wav");
+const fireballSFX = new Audio("sfx/player_sfx/player_attack/player_fireball.mp3");
 function playFireballSFX() {
     fireballSFX.currentTime = 0;
     fireballSFX.play().catch(e => console.log("Audio playback prevented:", e));
